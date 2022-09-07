c70cdc09d9fbc18510d98e76b2abddd44e399845
rvanarsdall <rvanar@gmail.com>
2022-09-07 00:33:33 -0400

    fixed for students

--- public/index.js
+++ public/index.js
@@ -7,26 +7,6 @@ form.onsubmit = handleSubmit;
 
 async function handleSubmit(e) {
   e.preventDefault();
-  const url = "http://localhost:4000/test/test";
-  const data = JSON.stringify({
-    fullName: fullName.value,
-    email: email.value,
-    message: message.value,
-  });
-
-  let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: data,
-  };
-
-  let response = await fetch(url, requestOptions);
-  let responseJSON = await response.json();
-
-  console.log(responseJSON);
 }
 
 console.log("It worked");
