62c78aa99a8ff89c41c5c6c2a7936456167d3535
rvanarsdall <rvanar@gmail.com>
2022-09-07 00:42:09 -0400

    clean code for student

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Express = require("express");
 
+//TODO import email controller
+
 const app = Express();
-const email = require("./controllers/email");
-app.use(Express.json());
-app.use(Express.static(`${__dirname}/public`));
-app.use("/email", email);
+//TODO parses incoming requests with JSON payloads
+
+//TODO create a static page for public folder
+
+//TODO use the email controller "/email"
+
 app.listen(4000, () => {
   console.log(`server listening on 4000`);
 });
---
+++ controllers/email.js
@@ -0,0 +1,9 @@
+//TODO create a router and export the router
+
+//TODO create a send - POST endpoint
+
+//TODO send a response that is JSON with a message that sent ex: {message: "sent"}
+
+//TODO capture the req.body of fullName, email, & message
+
+//TODO in the JSON response add {message: "sent", bodyObject: {fullName, email, message}}
